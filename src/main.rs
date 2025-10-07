1fc59a2154ae6a8c83e8c9291e6dfd2d88d05562
lambtoken <milivojejankovic1996@gmail.com>
2025-10-08 00:24:07 +0200

    small fixes

--- src/main.rs
+++ src/main.rs
@@ -8,34 +8,32 @@ fn run_func(v: Vec<f32>, callback: fn(f32) -> f32) -> Vec<f32> {
     v.into_iter().map(|x| callback(x)).collect()
 }
 
-fn graph(dots: Vec<f32>) {
+fn graph(dots: &[f32]) {
     let width = dots.len();
     let mut height_top: i32 = 0;
-    let mut height_bottom: i32 = 0;
+    let mut height_bottom: i32 = i32::MAX;
     let height;
 
-    for n in &dots {
+    for n in dots {
         if *n == 0. {
             continue;
         }
 
-        let sign = n / n;
-
         // positive
-        if sign == 1. {
+        if *n > 0. {
             height_top = cmp::max(height_top, n.round() as i32);
         // negative
-        } else {
+        } else if *n < 0.{
             height_bottom = cmp::min(height_bottom, n.round() as i32);
         }
     }
 
-    height = height_top + height_bottom * -1;
+    height = height_top - height_bottom;
 
-    println!("{} and {}", width, height);
+    println!("width: {} height: {}", width, height);
 
-    for y in 1..=height {
-        for x in 1..width {
+    for y in (height_bottom..=height_top).rev() {
+        for x in 0..width {
             if y == dots[x].round() as i32 {
                 print!("x");
             } else {
@@ -52,16 +50,12 @@ fn main() {
 
     let mut n: f32 = 0.;
 
-    for _ in 0..32 {
-        nums.push(n.clone());
+    for _ in 0..64 {
+        nums.push(n);
         n += 0.1;
     }
 
     let nums_after: Vec<f32> = run_func(nums, fun);
 
-    for n in &nums_after {
-        println!("{} ", n);
-    }
-
-    graph(nums_after);
+    graph(&nums_after);
 }
